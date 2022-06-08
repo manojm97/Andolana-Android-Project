ade65a65725c2019717d3da00d74b965ec26507d
Manoj <manojm.developer@gmail.com>
2022-06-09 00:15:20 +0530

    Pushing android project code for Andolana site

---
+++ app/src/main/java/andolana/android/com/MyFirebaseMessageingService.java
@@ -0,0 +1,75 @@
+package andolana.android.com;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import androidx.core.app.NotificationCompat;
+
+import org.json.JSONObject;
+
+public class MyFirebaseMessageingService extends FirebaseMessagingService {
+    public MyFirebaseMessageingService() {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        super.onMessageReceived(remoteMessage);
+
+        sendNotification(remoteMessage.getNotification().getBody());
+    /*    @Override
+        public void onMessageReceived(RemoteMessage remoteMessage) {
+            if (remoteMessage.getData().size() > 0) {
+                Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+                try {
+                    JSONObject json = new JSONObject(remoteMessage.getData().toString());
+
+                    //getting the json data
+                    JSONObject data = json.getJSONObject("data");
+
+                    //parsing json data
+                    String title = data.getString("title");
+                    String message = data.getString("message");
+                    String imageUrl = data.getString("image");
+
+
+                } catch (Exception e) {
+                    Log.e(TAG, "Exception: " + e.getMessage());
+                }
+            }
+        } */
+    }
+
+    
+    private void sendNotification(String messageBody)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setSmallIcon(R.drawable.ic_stat_name);
+        notificationBuilder.setContentTitle("MMusic");
+        notificationBuilder.setContentText(messageBody);
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setSound(defaultSoundUri);
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+
+    }
+
+}
